dbd351036e2c705e113a4059a9df68365ff0ee66
Jashwanth S Poojary <149219296+JashwanthSPoojary@users.noreply.github.com>
2025-06-11 18:39:06 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -20,7 +20,7 @@ app.use("/uploads", express.static(path.join(__dirname, "..", "uploads")));
 app.use(express.json());
 app.use(
   cors({
-    origin: FRONTEND_URL,
+    origin: [ FRONTEND_URL, 'https://www.buzznet.me' ]
   })
 );
 app.use(passport.initialize());
